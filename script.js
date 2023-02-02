e4089cba63e44ec390bf4791fc76566f7c33e3c1
daspa30d <121801471+daspa30d@users.noreply.github.com>
2023-02-02 20:39:09 +0100

    insert

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-// JavaScript code goes here
+function getColor() {
+    console.log(document.getElementById("inputColor").value)
+}
+
+function getValore() {
+    console.log(document.getElementById("inputTesto").value)
+}
+
+function cambiaValore() {
+    document.getElementById("progresso").value = 50
+}
+
+function prendiValore() {
+    console.log(document.getElementById("testoEditabile").innerText)
+}
